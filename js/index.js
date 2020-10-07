507317aefcaf7d8fa8c075c181b1c209d629d6c1
gentsao <61525709+gentsaot@users.noreply.github.com>
2020-10-07 14:37:47 -0400

    Responsive homepage updates
    Still buggy. Need to:
    
    -Adjust js for mobile
    -Adjust layout for about section so that it isn't overlapping.

--- js/index.js
+++ js/index.js
@@ -1,36 +1,5 @@
 console.log("Your index.js has loaded correctly! ^_^");
 
-$(document).ready(function(){
-    $("#orchids").hover(function(){
-        $("#orchid-reveal").css("opacity","100%");
-    },
-    function(){
-        $("#orchid-reveal").css("opacity","0%")
-    });
-});
-
-$(document).ready(function(){
-    $("#paint").hover(function(){
-        $("#paint-reveal").css("opacity","100%");
-    },
-    function(){
-        $("#paint-reveal").css("opacity","0%")
-    });
-});
-
-$(document).ready(function(){
-    $("#cat").hover(function(){
-        $("#cat-reveal").css("opacity","100%");
-    },
-    function(){
-        $("#cat-reveal").css("opacity","0%")
-    });
-});
-
-$(window).scroll(function(){
-    $(".bounce").css("opacity", 1 - $(window).scrollTop() / 250); 
-  });
-
 (function ($) {
     $(function () {
         $('nav ul li > a:not(:only-child)').click(function (e) {
@@ -49,3 +18,21 @@ $(window).scroll(function(){
         });
     })
 })(jQuery);
+
+$(document).ready(function() {
+    $(window).scroll(function(){
+        $(".container").each(function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object){
+                $(this).animate({"opacity" : "1"}, 1000)
+            } 
+        });
+    });
+}); 
+
+$(window).scroll(function(){
+    $("#arrow1").css("opacity", 1 - $(window).scrollTop()  / 500);
+    $("#hero").css("opacity", 1 - $(window).scrollTop()  / 500);  
+    $("#arrow2").css("opacity", 2.05 - $(window).scrollTop() / 500);
+  });
